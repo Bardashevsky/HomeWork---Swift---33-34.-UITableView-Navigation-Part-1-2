2cebc1b6f5591eb606a8f48d4de7d7dc2cf9b12e
Alex Bardashevsky <mijiekc@icloud.com>
2018-11-23 09:58:53 +0200

    Initial Commit

---
+++ HomeWork - Swift - 33-34. UITableView Navigation Part 1-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork - Swift - 33-34. UITableView Navigation Part 1-2
+//
+//  Created by Oleksandr Bardashevskyi on 11/23/18.
+//  Copyright © 2018 Oleksandr Bardashevskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HomeWork - Swift - 33-34. UITableView Navigation Part 1-2Tests/HomeWork___Swift___33_34__UITableView_Navigation_Part_1_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HomeWork___Swift___33_34__UITableView_Navigation_Part_1_2Tests.swift
+//  HomeWork - Swift - 33-34. UITableView Navigation Part 1-2Tests
+//
+//  Created by Oleksandr Bardashevskyi on 11/23/18.
+//  Copyright © 2018 Oleksandr Bardashevskyi. All rights reserved.
+//
+
+import XCTest
+@testable import HomeWork___Swift___33_34__UITableView_Navigation_Part_1_2
+
+class HomeWork___Swift___33_34__UITableView_Navigation_Part_1_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
